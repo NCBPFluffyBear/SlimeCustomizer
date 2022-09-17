6cd2c5539437fbd87705b9dbca933087f2c54e77
ybw0014 <i@guizhanss.com>
2022-09-17 10:34:27 -0400

    Allow tier for all types of category

--- src/main/java/io/ncbpfluffybear/slimecustomizer/registration/Categories.java
+++ src/main/java/io/ncbpfluffybear/slimecustomizer/registration/Categories.java
@@ -1,16 +1,16 @@
 package io.ncbpfluffybear.slimecustomizer.registration;
 
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.groups.LockedItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.groups.NestedItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.groups.SeasonalItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.groups.SubItemGroup;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 import io.ncbpfluffybear.slimecustomizer.Registry;
 import io.ncbpfluffybear.slimecustomizer.SlimeCustomizer;
 import io.ncbpfluffybear.slimecustomizer.Utils;
-import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
@@ -40,6 +40,7 @@ public class Categories {
             String type = categories.getString(categoryKey + ".type");
             String name = categories.getString(categoryKey + ".category-name");
             String materialString = categories.getString(categoryKey + ".category-item");
+            String tierStr = categories.getString(categoryKey + ".tier");
             Material material = Material.getMaterial(materialString);
             ItemStack item = null;
 
@@ -68,9 +69,15 @@ public class Categories {
 
             ItemGroup tempCategory;
             NamespacedKey key = new NamespacedKey(SlimeCustomizer.getInstance(), itemGroupKey);
+            int tier;
+            try {
+                tier = Integer.parseInt(tierStr);
+            } catch (NumberFormatException ex) {
+                tier = 3;
+            }
 
             if (type.equalsIgnoreCase("nested")) {
-                tempCategory = new NestedItemGroup(key, item);
+                tempCategory = new NestedItemGroup(key, item, tier);
             } else if (type.equalsIgnoreCase("sub")) {
                 String parent = categories.getString(categoryKey + ".parent");
                 if (parent == null) {
@@ -84,10 +91,9 @@ public class Categories {
                     return false;
                 }
 
-                tempCategory = new SubItemGroup(key, (NestedItemGroup) parentGroup, item);
+                tempCategory = new SubItemGroup(key, (NestedItemGroup) parentGroup, item, tier);
             } else if (type.equalsIgnoreCase("seasonal")) {
                 int monthNum = categories.getInt(categoryKey + ".month");
-                int tier = categories.getInt(categoryKey + ".tier");
                 Month month;
 
                 try {
@@ -106,10 +112,10 @@ public class Categories {
                     parentKeys[i++] = NamespacedKey.fromString(parent, SlimeCustomizer.getInstance());
                 }
 
-                tempCategory = new LockedItemGroup(key, item, parentKeys);
+                tempCategory = new LockedItemGroup(key, item, tier, parentKeys);
             } else {
                 type = "normal";
-                tempCategory = new ItemGroup(key, item);
+                tempCategory = new ItemGroup(key, item, tier);
             }
 
             Registry.allItemGroups.put(itemGroupKey, tempCategory);
